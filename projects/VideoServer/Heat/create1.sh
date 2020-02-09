6042b1d879030d780879f0fbe617eefdb9c969ea
musadiqraees <musadiqraees@gmail.com>
2020-02-09 20:31:45 +0100

    loop done

---
+++ projects/VideoServer/Heat/create1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CWD="$(pwd)"
+declare -a arrayname=( CloudCentral Edge1 Edge2 FarEdge )
+for i in "${arrayname[@]}";
+do
+   source $CWD/"$i"/edge-use-cases-openrc.sh
+   ansible-playbook deployserver.yml --extra-vars="myvarfile=$CWD/"$i"/variable.json"
+
+
+   echo $CWD/"$i"/\"@variable.json\"
+ done
