2e4c31fe875d313dfb3d38ac943821d29ee87dd6
musadiqraees <musadiqraees@gmail.com>
2020-02-10 04:35:26 +0100

    deployed peertube and dns

--- projects/VideoServer/Heat/create1.sh
+++ projects/VideoServer/Heat/create1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 CWD="$(pwd)"
-declare -a arrayname=( CloudCentral Edge1 Edge2 FarEdge )
+declare -a arrayname=( CloudCentral Edge1 Edge2 )
 for i in "${arrayname[@]}";
 do
    source $CWD/"$i"/edge-use-cases-openrc.sh
    ansible-playbook deployserver.yml --extra-vars="myvarfile=$CWD/"$i"/variable.json"
-
-
-   echo $CWD/"$i"/\"@variable.json\"
  done
+
+ansible-playbook updateDns.yml
+ansible-playbook deployDocker.yml
+
+ansible-playbook deploypeertubes.yml
